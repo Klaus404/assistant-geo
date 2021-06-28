7b89fc91640fc91aa71d363cc5fde09619c1e182
Barbu Claudiu <barbuclaudiu404@gmail.com>
2021-06-28 18:29:12 +0300

    Am separat main de GUI

--- GUI.py
+++ GUI.py
@@ -1,19 +1,33 @@
-import main
+import brain
 import tkinter as tk
-
+from tkinter import *
+# from PIL import ImageTk, Image
+# import jupyter
 
 root = tk.Tk()
+root.title('Geo assistant')
 
-canvas = tk.Canvas(root, height=700, width=700, bg="Orange")
-canvas.pack()
+root.resizable(width=True, height=True)
+root.geometry("1920x1080")
 
-frame = tk.Frame(root, bg="Black")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+bg = PhotoImage(file="C:/Users/Claudiu/PycharmProjects/chatBotMe/assistant.png")
+label1 = Label(root, image=bg)
+label1.place(x=0, y=0)
 
-runAss = tk.Button(root, text="Run Geo", padx=10, pady=5, fg="Black", bg="White", command=main.go_Geo)
+# text = Text(root, height=10, width=53)
+# text.place()
+
+# canvas = tk.Canvas(root, height=700, width=700, bg="Orange")
+# canvas.pack()
+
+# frame = tk.Frame(root, bg="Black")
+# frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+
+runAss = tk.Button(root, text="Run Geo", padx=10, pady=5, fg="Black", bg="White", command=brain.go_Geo, font="Jomhuria")
 runAss.pack()
 
-exit_program = tk.Button(root, text="Exit", padx=10, pady=5, fg="Black", bg="White", command=exit)
+exit_program = tk.Button(root, text="Exit", padx=10, pady=5, fg="Black", bg="White", command=quit, font="Jomhuria")
 exit_program.pack()
 
 root.mainloop()
